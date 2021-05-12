19fe5aa06fa0c8110a5da90dc9d1a156c476f48e
reverie <h221391ba@gmail.com>
2021-05-12 22:54:21 +0800

    2021/05/12 add fa-eye control( text <=> password)

--- js/MyTest_Table.js
+++ js/MyTest_Table.js
@@ -5,6 +5,8 @@ var emailLeg = document.getElementById('emailLeg');
 var password = document.getElementById('password');
 var pswLeg = document.getElementById('pswLeg');
 var pswFiel = document.getElementById('pswFiel');
+var faEye = document.getElementById('faEye');
+
 
 
 function test2 () {
@@ -22,12 +24,38 @@ function test2 () {
         emailFiel.style.border = "1px solid #afafaf";
         emailLeg.style.color = "#afafaf";
     }else{
-        console.log("both are not");
+        console.log("reset");
         emailFiel.style.border = "1px solid #afafaf";
         emailLeg.style.color = "#afafaf";
         pswFiel.style.border = "1px solid #afafaf";
         pswLeg.style.color = "#afafaf";
     }
 }
+
+faEye.addEventListener('click', eyeTgl);
 window.addEventListener('click', test2);
-window.addEventListener('keydown', test2)
+window.addEventListener('keydown', test2);
+
+// =========================fa-eye control============================
+
+function eyeTgl () {
+    console.log('123');
+    if ( password.type === 'password') {
+        password.type = 'text';
+        faEye.src = '../img/eye.svg';
+    }else{
+        password.type = 'password';
+        faEye.src = '../img/invisible.svg';
+    }
+}
+// let togglePassword = document.getElementById('togglePassword')
+// $('togglePassword').click(eyeTgl());
+
+// function eyeTgl () {
+
+//     const type = psw.getAttribute('type') === 'password' ? 'text' : 'password';
+//     // let pswType = $('password').getAttribute('type');
+//     $('#togglePassword').toggleClass('fa-eye-slash');
+// }
+
+console.log(faEye);
